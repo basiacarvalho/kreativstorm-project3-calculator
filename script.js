42a13242eed6e71dc3ee23b2bc9afd15653b586e
basiacarvalho <basiacarvalho@gmail.com>
2024-02-12 16:01:59 +0100

    creating operate function

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
-const display = document.querySelector(".calculator__display");
 
-// function showOnDisplay(input) {
-//   display.value += input;
-// }
+
+
 
 // function clearAllDisplay() {
 //   display.value = "";
@@ -31,26 +29,51 @@ function add(number1, number2) {
   return number1 + number2;
 }
 
-console.log(add(10, 5));
+// console.log(add(10, 5));
 
 // substract
 function substract(number1, number2) {
   return number1 - number2;
 }
 
-console.log(substract(10, 5));
+// console.log(substract(10, 5));
 
 // multiply
 function multiply(number1, number2) {
   return number1 * number2;
 }
 
-console.log(multiply(10, 5));
+// console.log(multiply(10, 5));
 
 // divide
 function divide(number1, number2) {
   return number1 / number2;
 }
 
-console.log(divide(10, 5));
+// console.log(divide(10, 5));
 
+function operate(operator, number1, number2) {
+  if (operator === "+") {
+    return add(number1, number2);
+  } else if (operator === "-") {
+    return substract(number1, number2);
+  } else if (operator === "*") {
+    return multiply(number1, number2);
+  } else if (operator === "/") {
+    return divide(number1, number2);
+  } else {
+    throw new Error("Unrecognized operator.");
+  };
+}
+
+// console.log(operate("+", 10, 5));
+
+const display = document.querySelector(".calculator__display");
+
+let displayValue = "0";
+
+function showOnDisplay(input) {
+  display.value += input;
+  displayValue = display.value;
+
+}
