5b556450a5051ff25f87ef8e383c31866b4a3786
basiacarvalho <basiacarvalho@gmail.com>
2024-02-12 15:42:14 +0100

    creating functions for all the basic math operators

--- script.js
+++ script.js
@@ -1,33 +1,56 @@
 const display = document.querySelector(".calculator__display");
 
-function showOnDisplay(input) {
-  display.value += input;
-}
+// function showOnDisplay(input) {
+//   display.value += input;
+// }
 
-function clearAllDisplay() {
-  display.value = "";
-}
+// function clearAllDisplay() {
+//   display.value = "";
+// }
 
-function calculate() {
-  try {
-    display.value = eval(display.value);
-  }
-  catch (error) {
-    display.value = "Error";
-  }
-}
+// function calculate() {
+//   try {
+//     display.value = eval(display.value);
+//   }
+//   catch (error) {
+//     display.value = "Error";
+//   }
+// }
 
-function clearLastDisplayElement() {
-
-}
+// function clearLastDisplayElement() {
+// }
 
 // clearAllDisplay
 // clearLastDisplayElement
-// divide
-// multiply
-// substract
-// add
+
 // calculate
 
 
+// add
+function add(number1, number2) {
+  return number1 + number2;
+}
+
+console.log(add(10, 5));
+
+// substract
+function substract(number1, number2) {
+  return number1 - number2;
+}
+
+console.log(substract(10, 5));
+
+// multiply
+function multiply(number1, number2) {
+  return number1 * number2;
+}
+
+console.log(multiply(10, 5));
+
+// divide
+function divide(number1, number2) {
+  return number1 / number2;
+}
+
+console.log(divide(10, 5));
 
